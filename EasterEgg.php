5a13e3371d9ee0c229ac08a63f77b23501a80c65
ElouannLN <e.le-nezet@laposte.net>
2020-02-18 00:18:23 +0100

    jsp

--- EasterEgg.php
+++ EasterEgg.php
@@ -2,9 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>YOUPI</title>
   </head>
   <body>
-    
+  Dansons la carioca
+
+
+  C'EST BIIENNNN
+
+
+  Faisez tous comme moi !!!!!  
   </body>
 </html>
