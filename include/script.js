ceb99acfff0e2a0e1d54fe6b49430fdaf9f0ab01
miszczuk-cell <miszczuk.ivan@gmail.com>
2020-02-17 21:58:58 +0100

    yututytuyutyuyu

--- include/script.js
+++ include/script.js
@@ -1,9 +1,15 @@
-var carte = document.querySelector(".carte");
 
+var compteur = 0;
+var carte = document.querySelector(".carte");
 var boutonCreation = document.querySelector(".btnCreation");
 var boutonConnexion = document.querySelector(".btnConnexion");
 boutonCreation.addEventListener("click",function(e){
   carte.style.transform = "rotateY(180deg)";
+  compteur = compteur + 1;
+  if(compteur > 4)
+  {
+    var = document.querySelector("body").style.backgroundColor ="red";
+  }
 });
 boutonConnexion.addEventListener("click",function(e){
   carte.style.transform = "rotateY(0deg)";
