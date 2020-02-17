7cada2359efd45a99a143ced7b57a1ddd90c1dc2
miszczuk-cell <miszczuk.ivan@gmail.com>
2020-02-17 22:30:16 +0100

    Just Erwan

--- include/script.js
+++ include/script.js
@@ -1,21 +1,26 @@
-
-var imageErwan = document.createElement("img");
-imageErwan.src = "image/erwan.jpg";
-var recto = document.querySelector(".recto");
-var compteur = 0;
-var carte = document.querySelector(".carte");
-var boutonCreation = document.querySelector(".btnCreation");
-var boutonConnexion = document.querySelector(".btnConnexion");
-boutonCreation.addEventListener("click",function(e){
-  carte.style.transform = "rotateY(180deg)";
-  compteur = compteur + 1;
-  if(compteur > 3)
+(function() {
+  var imageErwan = document.createElement("img");
+  imageErwan.src = "image/erwan.jpg";
+  imageErwan.className = "test";
+  var recto = document.querySelector(".recto");
+  var compteur = 0;
+  var carte = document.querySelector(".carte");
+  var boutonCreation = document.querySelector(".btnCreation");
+  var boutonConnexion = document.querySelector(".btnConnexion");
+  boutonCreation.addEventListener("click", function(e)
   {
-    var formulaire = document.querySelector(".recto form");
-    formulaire.remove();
-    recto.appendChild(imageErwan);
-  }
-});
-boutonConnexion.addEventListener("click",function(e){
-  carte.style.transform = "rotateY(0deg)";
-});
+    carte.style.transform = "rotateY(180deg)";
+    compteur = compteur + 1;
+    if (compteur > 1)
+    {
+      var formulaire = document.querySelector(".recto form");
+      formulaire.remove();
+      recto.appendChild(imageErwan);
+      document.querySelector(".recto h1").remove();
+    }
+  });
+  boutonConnexion.addEventListener("click", function(e)
+  {
+    carte.style.transform = "rotateY(0deg)";
+  });
+})();
