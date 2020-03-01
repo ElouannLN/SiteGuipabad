b58a883f000f6b9acfbd2d5c0df35416573ee4de
ElouannLN <e.le-nezet@laposte.net>
2020-03-01 19:37:46 +0100

    Organisation de la page d'accueil, il manque les fonds

--- accueil.php
+++ accueil.php
@@ -9,6 +9,7 @@
     <?php include("include/menu.php"); ?>
 
       <h2 class="actusbref">L'actu en bref !</h2>
+      <div class="textebrefetinfosclub">
         <p class="textactusbref">- Le club de Coatodon est enfin dissous ! <br/>
                                  - Plumy enfin dèrrière les barreaux ? <br/>
                                  - Le badminton, nouveau symbole du communisme.</p>
@@ -17,6 +18,7 @@
                                  Quand ? Pour les jeunes, le mardi de 18H30 à 20H. <br/>
                                  Pour les Adultes, les mardis et jeudis de 20H30 à 22H30. <br/>
                                  Plus d'informations <a href="inscriptions.php">ici</a>.</p>
+      </div>
 
       <h1 class="actualités">Actualités :</h1>
         <p class="news">Wow c'est incroyable Lannilis a encore perdu Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. lorem*15
