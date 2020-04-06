ad4c6e53fd214ab65408c4781f45daa53668a737
ElouannLN <e.le-nezet@laposte.net>
2020-04-07 01:32:24 +0200

    Correction  pour intégration du nouveau menu

--- include/menu.php
+++ include/menu.php
@@ -1,22 +1,23 @@
 <div class="menu">
 
-  <div class="menufix">
+  <div class="supmenu">
 
     <a class="lienlogosite" href="accueil.php">
       <img class="Logosite" src="image/Logosite.png" alt="Logo du site">
     </a>
 
-    <div class="liencote">
-      <a class="lienlogoconnexion" href="authentification.php">
-        <img class="LogoConnexion" src="image/LogoConnexion.png" alt="Logo d'authentification">
-      </a>
-      <a class="lienlogoconnexion" href="authentification.php">
-        <img class="LogoConnexion" src="image/LogoConnexion.png" alt="Logo d'authentification">
-      </a>
+  </a>
+  <a class="lienlogoconnexion" href="authentification.php">
+    <img class="LogoConnexion" src="image/LogoConnexion.png" alt="Logo d'authentification">
+  </a>
+
+  <div class="liencote">
+    <a class="lienlogoconnexion" href="authentification.php">
+      <img class="LogoConnexion" src="image/LogoConnexion.png" alt="Logo d'authentification">
     </div>
   </div>
 
-  <div class="menul">
+  <div class=undermenu>
     <a class="menufixprop" href="accueil.php">Accueil</a>
     <a class="menufixprop" href="tournois.php">Tournois</a>
     <a class="menufixprop" href="contacts.php">Contacts</a>
