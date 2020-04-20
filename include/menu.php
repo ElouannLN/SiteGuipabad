bcbb2525f58a62449643dd2e148035b99432e374
ElouannLN <e.le-nezet@laposte.net>
2020-04-20 15:52:54 +0200

    UPDATE MENU

--- include/menu.php
+++ include/menu.php
@@ -6,24 +6,20 @@
       <img class="Logosite" src="image/LogoNMenu.png" alt="Logo du site">
     </a>
 
-  </a>
   <a class="lienlogoconnexion" href="authentification.php">
     <img class="LogoConnexion" src="image/LogoConnexion.png" alt="Logo d'authentification">
   </a>
+  </div>
 
-  <div class="liencote">
-    <a class="lienlogocote" href="authentification.php">
-      <img class="LogoCote" src="image/LogoConnexion.png" alt="Logo d'authentification">
-    </div>
   </div>
 
   <div class=undermenu>
-    <a class="menufixprop" href="accueil.php">Accueil</a>
-    <a class="menufixprop" href="tournois.php">Tournois</a>
-    <a class="menufixprop" href="contacts.php">Contacts</a>
-    <a class="menufixprop" href="inscriptions.php">Inscriptions</a>
-    <a class="menufixprop" href="photos.php">Galerie photos</a>
-    <a class="menufixprop" href="presse.php">Articles de presse</a>
+    <a class="menulien" href="accueil.php">Accueil</a>
+    <a class="menulien" href="tournois.php">Tournois</a>
+    <a class="menulien" href="contacts.php">Contacts</a>
+    <a class="menulien" href="inscriptions.php">Inscriptions</a>
+    <a class="menulien" href="photos.php">Galerie photos</a>
+    <a class="menulien" href="presse.php">Articles de presse</a>
   </div>
 
 </div>
