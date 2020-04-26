02e4bcda9e31f82477cc0e0f6e2e3476eefbe329
ElouannLN <e.le-nezet@laposte.net>
2020-04-26 02:44:32 +0200

    Avancée du menu, je finis demain :p

--- accueil.php
+++ accueil.php
@@ -2,28 +2,33 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Site internet de Guipavas Badminton | Bienvenue !</title>
+      <title>Site internet de Guipavas Badminton | Bienvenue !</title>
     <link rel="stylesheet" href="include/style.css">
   </head>
+
   <body>
     <?php include("include/menu.php"); ?>
 
-      <h2 class="actusbref">L'actu en bref !</h2>
-      <div class="textebrefetinfosclub">
-        <p class="textactusbref">- Le club de Coatodon est enfin dissous ! <br/>
-                                 - Plumy enfin dèrrière les barreaux ? <br/>
-                                 - Le badminton, nouveau symbole du communisme.</p>
+    <div class="textebrefetinfosclub">
+     <div class="caseactusbrefs">
+      <h2 class="actusbref">L'ACTU EN BREF !</h2>
+        <p class="textactusbref">Le club de Coatodon est enfin dissous ! <br/>
+                                 Plumy enfin dèrrière les barreaux ? <br/>
+                                 Le badminton, nouveau symbole du communisme.</p>
+     </div>
 
-        <p class="infosclubbref">Où ? à la Salle de Keranna, à Guipavas ! <br/>
-                                 Quand ? Pour les jeunes, le mardi de 18H30 à 20H. <br/>
-                                 Pour les Adultes, les mardis et jeudis de 20H30 à 22H30. <br/>
-                                 Plus d'informations <a href="inscriptions.php">ici</a>.</p>
-      </div>
+     <div class="Cartesetinfosclub">
+       <iframe class="Carteinfosclub" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1112.951534708599!2d-4.408116750624529!3d48.43619267272253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816b14d3a1b66c9%3A0x3d8da919d817dc80!2sSalle%20Omnisports%20de%20Keranna!5e0!3m2!1sfr!2sfr!4v1587860299407!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <p class="infosclub">Où ? à la Salle de Keranna, à Guipavas ! <br/>
+                            Quand ? Pour les jeunes, le mardi de 18H30 à 20H. <br/>
+                            Pour les Adultes, les mardis et jeudis de 20H30 à 22H30. <br/>
+                            Plus d'informations <a href="inscriptions.php">ici</a>.</p>
+     </div>
+    </div>
 
-      <h1 class="actualités">Actualités :</h1>
-        <p class="news">
-          </p>
-
-    <script type="text/javascript" scr="include/script.js"></script>
+     <h1 class="actualités">Actualités :</h1>
+      <p class="news">
+      </p>
+      <script type="text/javascript" scr="include/script.js"></script>
   </body>
 </html>
