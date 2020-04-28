7524f0b031fc79e7ebfc514b9177156af59cae4e
miszczuk-cell <miszczuk.ivan@gmail.com>
2020-04-28 12:03:51 +0200

    Affichage Tournoi

--- gestionTournoi.php
+++ gestionTournoi.php
@@ -13,6 +13,9 @@ if(isset($_SESSION["privilege"]))
 
             $SQL1 = "INSERT INTO tournoi (tournoi,club,categorie";
             $SQL2 = " VALUES (";
+            // SQL1 et SQL2 sont des chaînes de caractères qui vont être complétées
+            // en fonction des input remplis ou non (certains sont facultatifs).
+            // Assemblées ces 2 chaînes de caractères sont une requête INSERT INTO
 
             $tournoi = $_POST["ajouterTournoi"];
             $SQL2 = $SQL2 . "\"".$tournoi."\"";
@@ -44,14 +47,28 @@ if(isset($_SESSION["privilege"]))
                 $SQL2 = $SQL2 . ",".$cout;
             }
 
-            if($_POST["affiche"] != "")
-            {
-                $affiche = $_POST["affiche"];
-                $SQL1 = $SQL1 . ",affiche";
-                $SQL2 = $SQL2 . ",\"".$affiche."\"";
-                $uploaddir = 'afficheTournoi';
-                $uploadfile = $uploaddir . basename($_FILES['affiche']['name']);
-                move_uploaded_file($_FILES['affiche']['tmp_name'], $uploadfile);
+            $affiche = $_FILES['affiche']['name'];
+            $SQL1 = $SQL1 . ",affiche";
+            $SQL2 = $SQL2 . ",\"".$affiche."\"";
+            if(isset($_FILES['affiche'])) //Test si l'input file a été utilisé.
+            { 
+                $dossier = 'afficheTournoi/'; //Le dossier de destination du fichier.
+                //basename() sépare le nom du fichier et le nom du dossier.
+                $fichier = basename($_FILES['affiche']['name']); //récupère le nom du fichier.
+                if(move_uploaded_file($_FILES['affiche']['tmp_name'], $dossier . $fichier))
+                {
+                    //move_uploaded_file() déplace le fichier du répertoire temporaire vers le dossier voulu.
+                    //move_uploaded_file() renvoie TRUE si le fichier a bien été déplacé.
+                    //move_uploaded_file() prend 2 paramètres.
+                    //Le nom du fichier dans le répertoire temporaire et le chemin du dossier de destination
+                    //dans le répertoire temporaire le fichier prend un nom différent de celui à l'upload.
+                    //$_FILES['affiche']['tmp_name'] permet d'obtenir le nom du fichier dans le répertoire temporaire.
+                    //Ne pas oublier l'attribut enctype="multipart/form-data" du formulaire.
+                }
+                else //Sinon (la fonction renvoie FALSE).
+                {
+                    echo 'Echec de l\'upload !';
+                }
             }
 
             $SQL1 = $SQL1 . ") ";
@@ -59,7 +76,7 @@ if(isset($_SESSION["privilege"]))
 
             $cnx->query($SQL1.$SQL2);
             echo $SQL1.$SQL2;
-            //header("Location: tournois.php");
+            header("Location: tournois.php");
         }
         elseif (isset($_GET["suppression"]))
         {
---
+++ include/formAjoutTournoi.php
@@ -0,0 +1,43 @@
+<form enctype="multipart/form-data" action="gestionTournoi.php" method="POST" class="formAjouterTournoi" >
+      <table>
+        <tr>
+          <td><label for="tournoi">Nom du tournoi :</label></td>
+          <td><input type="text" id="tournoi" name="ajouterTournoi" required value="La Revanche"></td>
+        </tr>
+        <tr>
+          <td><label for="club">Nom du club</label></td>
+          <td><input type="text" id="club" name="club" value="Guipabad"></td>
+        </tr>
+        <tr>
+          <td><label for="categorie">Catégorie :</label></td>
+          <td>
+            <select name="categorie" id="categorie">
+              <option value="0">Jeune</option>
+              <option value="1">Adulte</option>
+            </select>
+        </td>
+        </tr>
+        <tr>
+          <td><label for="date">Date :</label></td>
+          <td><input type="date" id="date" name="date" required></td>
+        </tr>
+        <tr>
+          <td><label for="adresse">Adresse :</label></td>
+          <td><input type="text" id="adresse" name="adresse"></td>
+        </tr>
+        <tr>
+          <td><label for="cout">Coût d'inscription :</label></td>
+          <td><input type="number" id="cout" name="cout"></td>
+        </tr>
+        <tr>
+          <td><label for="affiche">Affiche :</label></td>
+          <td><input type="file" id="affiche" name="affiche"></td>
+          <input type="hidden" name="MAX_FILE_SIZE" value="300000000" />
+        </tr>
+        <tr>
+          <td></td>
+          <td><input type="submit"></td>
+        </tr>
+      </table>
+    </form>
+    <div class="separation"></div>
